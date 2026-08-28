56e10b7024454ee0455858a0c8ff864f56b00ad3
Pedro Azevedo <pazevedo.dev@gmail.com>
2026-08-28 08:26:58 -0300

    chore: remove scratch test/patch files from repo

--- test_feeds.ts
+++
@@ -1,8 +0,0 @@
-
-import { redis } from './src/Data/Redis.js';
-(async () => {
-    console.log('News:', await redis.get('config:feeds:news'));
-    console.log('Trending:', await redis.get('config:feeds:trending'));
-    process.exit(0);
-})();
-
